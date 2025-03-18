3a878ef4122c06c099c61c2be991410f38fc04e4
lucasoneves <casluhc@gmail.com>
2025-03-18 20:03:42 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+
+// To do: Create a button and the press function to show the alert with a message.
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity onPress={() => console.log("Something")}>
+          <Text>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2f2f2",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    padding: 12,
   },
+  itemContainer: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "1a759f",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    width: "100%",
+  },
+  itemText: {},
 });
